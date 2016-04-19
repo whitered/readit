34e1877f012a213cc97970c23765413cab2b62d6
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-04-19 16:41:18 -0500

    adds info to mix file for hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Readit.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "A Simple Read-Only Reddit API Client",
+     package: package,
      deps: deps]
   end
 
@@ -16,8 +18,16 @@ defmodule Readit.Mixfile do
 
   defp deps do
     [
-      {:poison, "~> 2.1"},
+      {:poison,    "~> 2.1"},
       {:httpoison, "~> 0.8"}
     ]
+  end
+
+  defp package do
+    [files: ["lib", "config", "mix.exs", "README*"],
+     maintainers: ["Adam Zaninovich"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/adamzaninovich/readit",
+              "Docs"   => "https://github.com/adamzaninovich/readit"}]
   end
 end
