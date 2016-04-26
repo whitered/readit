1a04a03a0a45a15dc2b8a1142f154545770b86f4
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-04-26 09:24:10 -0700

    adds test for Client

---
+++ test/readit/client_test.exs
@@ -0,0 +1,19 @@
+defmodule Readit.ClientTest do
+  use ExUnit.Case, async: true
+  alias Readit.Client
+
+  test "process_url callback adds the domain to the path" do
+    assert Client.process_url("/path") == "http://www.reddit.com/path"
+  end
+
+  test "process_response_body parses and maps the response (excluding unknown keys)" do
+    response_body = """
+    {"data":{
+      "children":[
+        {"data":{"url": "test.jpg", "other_thing": true}},
+        {"data":{"url": "test.txt"}}]}}
+    """
+    parsed = [%{url: "test.jpg"}, %{url: "test.txt"}]
+    assert parsed == Client.process_response_body(response_body)
+  end
+end
