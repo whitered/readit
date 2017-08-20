fc29acc3cea1ca451083002f8e430fcd7f586527
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-08-19 21:14:10 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Readit.Mixfile do
 
   def project do
     [app: :readit,
-     version: "0.0.4",
+     version: "0.1.0",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
